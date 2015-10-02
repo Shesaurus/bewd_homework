c68ffc805524b083532e693111fc497df48d5244
Marquisha Gatewood <mgatewood@shesaurus.com>
2015-10-02 15:48:54 -0700

    Update beers.rb

--- beers.rb
+++ beers.rb
@@ -1,1 +1,18 @@
+puts '99 bottles of beer'
+bottles = 99
 
+while(bottles > 1) do
+  puts "#{num_bottles} bottles of beer on the wall, #{bottles} bottles of beer."
+  bottles = bottles - 1
+  if bottles == 1
+    puts "Take one down and pass it around, #{bottles} bottle of beer on the wall."
+else 
+    puts "Take one down and pass it around, #{bottles} bottles of beer on the wall."
+    
+  end
+  
+end
+
+    
+  
+    
